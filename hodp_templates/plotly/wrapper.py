d830e53ac2b2e7293e8807d26692bef47156fe73
Kev-Y-Huang <kev.y.huang@gmail.com>
2021-02-14 15:16:51 -0500

    added bar graph wrapper (handle both stack and group)

--- hodp_templates/plotly/wrapper.py
+++ hodp_templates/plotly/wrapper.py
@@ -18,9 +18,9 @@ def line(data_frame, x, y, line_group, labels, title):
         ))
 
     fig.update_layout(title=title,
-                      xaxis={'title': {'text': labels[x]}},
-                      yaxis={'title': {'text': labels[y]}},
-                      legend={'title': {'text': labels[line_group]}},
+                      xaxis={'title': {'text': labels[x] if x in labels else x}},
+                      yaxis={'title': {'text': labels[y] if y in labels else y}},
+                      legend={'title': {'text': labels[line_group] if line_group in labels else line_group}},
                       template=hodp_theme)
 
     return fig
@@ -42,9 +42,34 @@ def scatter(data_frame, x, y, line_group, labels, title):
         ))
 
     fig.update_layout(title=title,
-                      xaxis={'title': {'text': labels[x]}},
-                      yaxis={'title': {'text': labels[y]}},
-                      legend={'title': {'text': labels[line_group]}},
+                      xaxis={'title': {'text': labels[x] if x in labels else x}},
+                      yaxis={'title': {'text': labels[y] if y in labels else y}},
+                      legend={'title': {'text': labels[line_group] if line_group in labels else line_group}},
                       template=hodp_theme)
+
+    return fig
+
+
+def bar(data_frame, x, y, line_group, labels, title, barmode='group'):
+    fig = go.Figure()
+
+    categories = data_frame[line_group].unique()
+
+    for i, category in enumerate(categories):
+        df = data_frame[data_frame[line_group] == category]
+        fig.add_trace(go.Bar(
+            x=df[x],
+            y=df[y],
+            hovertext=[f"<b>{labels[x] if x in labels else x}</b>={df[x][i]}<br>" for i in range(len(df))],
+            name=category,
+            marker_color=primary_colors[i % 5],
+        ))
+
+    fig.update_layout(title=title,
+                      xaxis={'title': {'text': labels[x] if x in labels else x}},
+                      yaxis={'title': {'text': labels[y] if y in labels else y}},
+                      legend={'title': {'text': labels[line_group] if line_group in labels else line_group}},
+                      template=hodp_theme,
+                      barmode=barmode)
 
     return fig
